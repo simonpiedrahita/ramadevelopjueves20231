6f85f3fb448332bd30e8ee8c7b093de4647c62e4
encualva <encualva@gmail.com>
2023-04-20 07:10:36 -0500

    NT 04/20 se agrega la fresa

---
+++ Fresa.py
@@ -0,0 +1,10 @@
+class Mango:
+    def __init__(self):
+        self.nombre = None
+        self.cantidad = None
+        self.estado = None
+        self.precio = None
+        
+    def describirFruta(self):
+        print(f"Mi fruta la fresa, es delicioso y sirve para hacer jugo ")
+        
