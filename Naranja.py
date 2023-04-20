18d7efec34fb276f5a2430f730f1824d541f17af
dcm323 <danielcalle323@gmail.com>
2023-04-20 06:59:57 -0500

    nt10 se crea fruta naranja

---
+++ Naranja.py
@@ -0,0 +1,10 @@
+class Naranja:
+
+    def __init__(self):
+        self.nombre=None
+        self.cantidad=None
+        self.estado=None
+        self.precio=None
+    
+    def describirFruta(self):
+        print(f'mi fruta naranja es deliciosa y sirve para aportar vitamina c')
