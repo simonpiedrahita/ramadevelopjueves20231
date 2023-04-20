d8cc07d3c5cba736d266c8f86b2d22f07763be87
encualva <encualva@gmail.com>
2023-04-20 08:41:28 -0500

    fin de clase

--- Fresa.py
+++ Fresa.py
@@ -1,10 +1,41 @@
-class Mango:
+class Fresa:
     def __init__(self):
-        self.nombre = None
-        self.cantidad = None
-        self.estado = None
-        self.precio = None
+        self.__nombre = None
+        self.__cantidad = None
+        self.__estado = None
+        self.__precio = None
+        
+    #GETTERS ( piden sacan o reciben la informacion)
+    @property
+    def nombre(self):
+        return self.__nombre
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    @property
+    def estado(self):
+        return self.__estado
+    @property
+    def precio(self):
+        return self.__precio
+    
+    #SETTER
+    @nombre.setter
+    def nombre(self,dato):
+        self.__nombre=dato
+    @cantidad.setter
+    def cantidad(self,dato):
+        self.__cantidad=dato
+    @estado.setter
+    def estado(self,dato):
+        self.__estado=dato
+    @precio.setter
+    def precio(self,dato):
+        self.__precio=dato
+        
         
     def describirFruta(self):
         print(f"Mi fruta la fresa, es delicioso y sirve para hacer jugo ")
+        
+    
         
---
+++ main.py
@@ -0,0 +1,13 @@
+from Fresa import Fresa
+
+objeto = Fresa()
+
+#agregandole un nombre a la naranja 
+#objeto.nombre  #get
+objeto.nombre="Freson"
+objeto.cantidad= 5
+objeto.estado= "melo"
+objeto.precio= 500
+print(objeto.nombre,objeto.cantidad,objeto.estado,"$",objeto.precio) 
+
+
