21b6614c4d5003d301fab7fcc5e5b834bf80d769
Nataliacardenas <ncardenasrestrepo@gmail.com>
2023-04-20 06:59:14 -0500

    NT10 Se crea fruta Manzana

---
+++ Manzana.py
@@ -0,0 +1,10 @@
+class Manzana:
+    
+    def __init__(self):
+        self.nombre = None
+        self.cantidad = None
+        self.estado = None
+        self.precio = None
+        
+    def describirFruta(self):
+        print(f'Mi fruta puede ser dulce o acida y crece en arboles')
