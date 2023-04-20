c99ad0fa3462c087fa308f805eb0eab348094bea
smarinv16 <funemesanchez@gmail.com>
2023-04-20 06:58:17 -0500

    Se agregan archivos rama featurejpg

---
+++ Mango.py
@@ -0,0 +1,12 @@
+class Mango:
+    def __init__(self):
+        self.nombre = None
+        self.cantidad = None
+        self.estado = None
+        self.precio = None
+        
+    def describirFruta(self):
+        print(f"Mi fruta el Mango, es delicioso y sirve para aportar vitamina B")
+        
+        
+   
