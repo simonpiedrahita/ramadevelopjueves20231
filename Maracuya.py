dc94e80ac4cba128e10f603f5e77a3120a66b414
Ruddy Rodriguez <ruddy.rodriguez.r@hotmail.com>
2023-04-20 07:07:41 -0500

    nt10 se crea clase Maracuya

---
+++ Maracuya.py
@@ -0,0 +1,10 @@
+class Maracuya:
+
+    def __init__(self):
+        self.nombre=None
+        self.cantidad=None
+        self.estado=None
+        self.precio=None
+
+    def describirFruta(self):
+        print(f'mi fruta Maracuya es deliciosa y sirve para darle acidez al salpicon')
